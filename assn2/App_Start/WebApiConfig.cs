56734f1a4b13ff24aa1a4715feeeb20e4ccf333a
Shah Newaz Khan <shahnewazk@gmail.com>
2015-04-06 07:49:46 -0700

    Changed API response format to expose JSON
    Signed-off-by: Shah Newaz Khan <shahnewazk@gmail.com>

--- assn2/App_Start/WebApiConfig.cs
+++ assn2/App_Start/WebApiConfig.cs
@@ -11,6 +11,10 @@ namespace assn2
         {
             config.MapHttpAttributeRoutes();
 
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
