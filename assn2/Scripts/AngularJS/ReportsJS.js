f7aa3e4e53f0671437a511b4cd7229bcd4b89b01
Shah Newaz Khan <shahnewazk@gmail.com>
2015-04-06 19:25:15 -0700

    Added ReportsJS and binded it to Reports/Index.cshtml
    Signed-off-by: Shah Newaz Khan <shahnewazk@gmail.com>

---
+++ assn2/Scripts/AngularJS/ReportsJS.js
@@ -0,0 +1,28 @@
+﻿var app;
+
+(function () {
+    app = angular.module("reportsAPI", []);
+})();
+
+app.config(['$httpProvider', function ($httpProvider) {
+    // Intercept POST requests, convert to standard form encoding
+    $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    $httpProvider.defaults.headers.put["Content-Type"] = "application/x-www-form-urlencoded";
+    $httpProvider.defaults.transformRequest.unshift(function (data, headersGetter) {
+        var key, result = [];
+        for (key in data) {
+            if (data.hasOwnProperty(key)) {
+                result.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
+            }
+        }
+        return result.join("&");
+    });
+}]);
+
+app.controller('getClients',
+function ($scope) {
+
+    $scope.test = "Hello world";
+    
+});
+
